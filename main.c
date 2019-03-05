45e1906eb2a8e3cd010584d01d68bb9aab5b1c70
ihebhamdi <47664538+ihebhamdi@users.noreply.github.com>
2019-03-05 11:51:36 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <string.h>
+#include "menu.h"
+
+int main(int argc , char* args[])
+{
+	initialiser();
+	affichage();
+	update();
+}
+
